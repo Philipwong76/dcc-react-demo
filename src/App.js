d0e52ed4f21672bd8fc11261c8e8d908ea336319
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-01-23 19:54:07 -0500

    Completed Entryform Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-23-2021'}])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm AddNewEntryProperty={addNewEntry} />
 
     </div>
   );
---
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+const AddEntryForm = (props) => {
+
+    const [weight, setWeight] = useState(0);
+    const [date, setDate] =useState('');
+
+    function handleSumbit(event) {
+        event.preventDefault();
+        let newEntry = {
+            weight: weight,
+            date: date
+        };
+        console.log(newEntry);
+        props.AddNewEntryProperty(newEntry)
+    }
+
+    return ( 
+        <form onSubmit={handleSumbit}>
+            <label>Weight</label>
+            <input type='number' value={weight} onChange={(event) => setWeight(parseFloat(event.target.value))}/>
+            <label>Date</label>
+            <input type ='date' value={date} onChange={(event) => setDate(event.target.value)}/>
+            <button type='submit'>Add</button>
+        </form>
+     );
+}
+ 
+export default AddEntryForm;
